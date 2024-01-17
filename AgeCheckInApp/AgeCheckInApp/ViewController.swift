e333e5fd24adb2985bfe9531f4612c92bdcf3a45
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-17 15:12:14 +0100

    Basic structure MVVM for the APP

---
+++ AgeCheckInApp/AgeCheckInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AgeCheckInApp
+//
+//  Created by Daniel Cazorro Frias  on 17/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
