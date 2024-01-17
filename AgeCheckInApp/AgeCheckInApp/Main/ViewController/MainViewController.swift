392e5260d63acf5b4814a5a2f982f214dce45318
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-17 15:38:27 +0100

    Add IBOutlets, IBActions and hidden school button default

--- AgeCheckInApp/AgeCheckInApp/Main/ViewController/MainViewController.swift
+++ AgeCheckInApp/AgeCheckInApp/Main/ViewController/MainViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameSurnameSV: UIStackView!
+    @IBOutlet weak var schoolObservationsSV: UIStackView!
+    @IBOutlet weak var finishResetButtonsSV: UIStackView!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surNameTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var schoolTextField: UITextField!
+    @IBOutlet weak var observationsTextField: UITextField!
+    
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    
+    @IBAction func tapFinishButton(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func tapResetButton(_ sender: Any) {
+    }
+    
+    
 }
